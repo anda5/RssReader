aff0a67c954663a204f626d551e5c4ebc2399284
Anda.Lixandroiu <lix.anda@yahoo.com>
2015-03-06 17:29:17 +0200

    Add getWebsiteDetails and getRssFeeds

--- app/src/androidTest/java/com/anda/rssreader/RssParserTest.java
+++ app/src/androidTest/java/com/anda/rssreader/RssParserTest.java
@@ -3,13 +3,12 @@ package com.anda.rssreader;
 import android.test.InstrumentationTestCase;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by anda on 2/10/2015.
  */
 public class RssParserTest extends InstrumentationTestCase {
-
-
 
     public void testGetRssLinkFomURL(){
         RssParser rssParser = new RssParser();
@@ -19,18 +18,41 @@ public class RssParserTest extends InstrumentationTestCase {
 
     public void testGetXMLFromURL(){
         RssParser rssParser = new RssParser();
-        String rss = rssParser.getXMLfromURL("http://www.feedforall.com/sample-feeds.htm");
-        assertEquals("http://www.feedforall.com/blog-feed.xml",rssParser);
+        String xml = rssParser.getXMLFromURL("http://www.feedforall.com/blog-feed.xml");
+        assertTrue(xml.startsWith("<?xml"));
     }
 
-    public void testGetWebSiteDetails(){
+    public void testWebsiteAndRssFeed () {
+        WebSite site = new WebSite();
+        site.setSiteLink("http://www.feedforall.com/sample-feeds.htm");
+        site.setFeedLink("http://www.feedforall.com/blog-feed.xml");
+
         RssParser rssParser = new RssParser();
-        WebSite website =rssParser.getWebSiteDetails("http://www.feedforall.com/sample-feeds.htm");
-        String title = website.getTitle();
-        String description = website.getDescription();
-        assertEquals("An RSS Daily News Feed from FeedForAll - RSS Feed Creation",title);
-        assertEquals("RSS is a fascinating technology",description);
-    }
+        String xml = rssParser.getXMLFromURL("http://www.feedforall.com/blog-feed.xml");
 
+        site = rssParser.getWebsiteDetails(site, xml);
+        site.setId(2);
+
+        // Assert website
+        assertEquals(site.getId(), 2);
+        assertEquals(site.getTitle(), "An RSS Daily News Feed from FeedForAll - RSS Feed Creation.");
+        assertTrue(site.getDescription().startsWith("RSS is a fascinating"));
+        assertEquals(site.getSiteLink(), "http://www.feedforall.com/sample-feeds.htm");
+        assertEquals(site.getFeedLink(), "http://www.feedforall.com/blog-feed.xml");
+
+        List<RssFeed> feedList = rssParser.getFeedDetails(site, xml);
+
+        // Assert feed
+        assertNotNull(feedList);
+        assertTrue(feedList.size() > 0);
+
+        for (RssFeed feed : feedList) {
+            assertTrue(feed.getWebSiteId() == 2);
+            assertFalse(feed.getTitle().isEmpty());
+            assertFalse(feed.getDescription().isEmpty());
+            assertTrue(feed.getFeedLink().startsWith("http://"));
+            assertFalse(feed.getDate().isEmpty());
+        }
+    }
 
 }
--- app/src/main/java/com/anda/rssreader/RssParser.java
+++ app/src/main/java/com/anda/rssreader/RssParser.java
@@ -2,6 +2,7 @@ package com.anda.rssreader;
 
 import android.provider.ContactsContract;
 import android.provider.DocumentsContract;
+import android.util.Log;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -10,6 +11,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 import org.w3c.dom.Document.*;
 import org.w3c.dom.Element;
@@ -22,6 +24,7 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -33,147 +36,80 @@ import javax.xml.parsers.ParserConfigurationException;
  */
 public class RssParser {
 
-    private static final String TAG_WEBSITE_CHANEL = "chanel";
-    private static final String TAG_WEBSITE_TITLE = "title";
-    private static final String TAG_WEBSITE_DESCRIPTION = "description";
-    private static final String TAG_WEBSITE_LINK = "link";
-    private static final String TAG_FEED_ITEM = "item";
-    private static final String TAG_FEED_TITLE = "title";
-    private static final String TAG_FEED_DESCRIPTION = "description";
-    private static final String TAG_FEED_LINK = "link";
-    private static final String TAG_FEED_DATE = "pubDate";
-    private static final String TAG_FEED_IMAGE = "image";
+    public RssParser() {}
 
-  public RssParser(){
-  }
+    public String getRssLinkFromURL(String url) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements rssLink = doc.select("link[type=application/rss+xml]");
+        Elements atomLink = doc.select("link[type=application/atom+xml]");
+        if(rssLink.size()>0){
+            return rssLink.attr("href");}
+        else if(atomLink.size()>0) {
+            return atomLink.attr("href");
+        }
+        else {
+            return null;
+        }
+    }
 
-  public String getRssLinkFromURL(String url){
-
-
-      Document doc = null;
-      try {
-          doc = Jsoup.connect(url).get();
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-      Elements link = doc.select("link[type=application/rss+xml]");
-          Elements linkt = doc.select("link[type=application/atom+xml]");
-          if(link.size()>0){
-              return link.attr("href").toString();}
-          else if(linkt.size()>0) {
-              return linkt.attr("href").toString();
-          }
-          else{
-              return null;
-          }
-  }
-    public String getXMLfromURL(String feedUrl){
-     try{
-        DefaultHttpClient client = new DefaultHttpClient();
-        HttpEntity entity = null;
-        HttpResponse response = null;
-        HttpGet httpGet = new HttpGet(feedUrl);
-        response = client.execute(httpGet);
-        entity = response.getEntity();
-        String  responseString = EntityUtils.toString(entity);
-        return responseString;
+    public String getXMLFromURL(String feedUrl) {
+        try {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(feedUrl);
+            HttpResponse response = client.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
-    
-    public Document getDomElement(String xml)  {
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder  documentBuilder = null;
-        try {
-            documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-        InputSource imputSource = new InputSource();
-        imputSource.setCharacterStream(new StringReader(xml));
-        try {
-            return (Document)documentBuilder.parse(imputSource);
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        return null;
+    public WebSite getWebsiteDetails (WebSite site, String xml) {
+        Document doc = Jsoup.parse(xml, "", Parser.xmlParser());
+        Elements title = doc.select("channel > title");
+        Elements description = doc.select("channel > description");
+
+        for (int i = 0; i < title.size(); i++) {
+            site.setTitle(title.get(i).text());
+            site.setDescription(description.get(i).text());
+        }
+        return site;
     }
 
-    public String getElementValue (Node elem){
-        if(elem != null){
-            if(elem.hasChildNodes()){
-                for(Node child = elem.getFirstChild();child != null;child = child.getNextSibling()){
-                    if(child.getNodeType()== Node.TEXT_NODE || child.getNodeType()== Node.CDATA_SECTION_NODE){
-                        return child.getNodeValue();}
+    public List<RssFeed> getFeedDetails (WebSite site, String xml) {
+        List<RssFeed> rssList = new ArrayList<>();
+        Document doc = Jsoup.parse(xml, "", Parser.xmlParser());
 
-                }
+        Elements title = doc.select("item > title");
+        Elements description = doc.select("item > description");
+        Elements link = doc.select("item > link");
+        Elements date = doc.select("item > pubDate");
+        Elements image = doc.select("item > image > url");
+
+        for (int i = 0; i < title.size(); i++) {
+            RssFeed feed = new RssFeed();
+            feed .setWebSiteId(site.getId());
+
+            feed.setTitle(title.get(i).text());
+            feed.setDescription(description.get(i).text());
+            feed.setFeedLink(link.get(i).text());
+            feed.setDate(date.get(i).text());
+
+            if (image.size() > 0) {
+                feed.setImageURL(image.get(i).text());
+            } else {
+                feed.setImageURL("");
             }
+            rssList.add(feed);
         }
-        else{
-            return "";
-        }
-        return null;
+        return rssList;
     }
-    
-    public String getValue(Element item , String str){
-        NodeList nodeList = item.getElementsByTagName(str);
-        return  getElementValue(nodeList.item(0));
-
-    }
-
-    public WebSite getWebSiteDetails(String url){
-      String URL = getRssLinkFromURL(url);
-      String xml = getXMLfromURL(URL);
-        if(URL!=null){
-            if(xml!=null) {
-      Document document = getDomElement(xml);
-      NodeList nodeList = (NodeList) document.getElementsByTag(TAG_WEBSITE_CHANEL);
-      Element element = (Element) nodeList.item(0);
-      String title = getValue(element,TAG_FEED_TITLE);
-      String description =  getValue(element,TAG_WEBSITE_DESCRIPTION);
-      String link = getValue(element,TAG_WEBSITE_LINK);
-      WebSite webSite = new WebSite();
-      webSite.setSiteLink(url);
-      webSite.setFeedLink(URL);
-              return webSite;
-          }
-        }
-        return null;
-    }
-
-    public List<RssFeed> getRssFeeds(String feedsURL){
-     List<RssFeed> rssFeedsList = null;
-     String xml = getXMLfromURL(feedsURL);
-     if(xml != null) {
-         Document document = getDomElement(xml);
-         NodeList nodeList = (NodeList) document.getElementsByTag(TAG_WEBSITE_CHANEL);
-         Element element = (Element) nodeList.item(0);
-         NodeList nodeList1 = element.getElementsByTagName(TAG_FEED_ITEM);
-         for (int node = 0; node < nodeList.getLength(); node++) {
-             String title = getValue((Element) nodeList1.item(node), TAG_FEED_TITLE);
-             String description = getValue((Element) nodeList1.item(node), TAG_FEED_DESCRIPTION);
-             String link = getValue((Element) nodeList1.item(node), TAG_FEED_LINK);
-             String date = getValue((Element) nodeList1.item(node), TAG_FEED_DATE);
-             String image = getValue((Element) nodeList1.item(node), TAG_FEED_IMAGE);
-             RssFeed rssFeed = new RssFeed();
-             rssFeed.setTitle(title);
-             rssFeed.setDescription(description);
-             rssFeed.setFeedLink(link);
-             rssFeed.setImageURL(image);
-             rssFeed.setDate(date);
-             rssFeedsList.add(rssFeed);
-
-         }
-         return rssFeedsList;
-     }
-    return null;
-
-    }
-
 
 }
+
