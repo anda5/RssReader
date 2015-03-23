dee69ef7c84ae53f905f75c0bd67dfcb2fbfd281
Anda.Lixandroiu <lix.anda@yahoo.com>
2015-03-23 22:43:21 +0200

    Add some changes of WebSiteActivity class ,fixed #96

--- app/src/main/java/com/anda/rssreader/WebSiteActivity.java
+++ app/src/main/java/com/anda/rssreader/WebSiteActivity.java
@@ -15,70 +15,58 @@ import android.widget.Toast;
 public class WebSiteActivity extends ActionBarActivity {
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_site);
-       final EditText editText = (EditText) findViewById(R.id.webSiteLink);
-       Button button = (Button) findViewById(R.id.webSiteButton);
-       button.setOnClickListener(new View.OnClickListener() {
+        final EditText editText = (EditText) findViewById(R.id.webSiteLink);
+        Button button = (Button) findViewById(R.id.webSiteButton);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!isEmpty(editText)) {
-                    try {
-                      String userLink = editText.toString();
-                        RssParser rssParser = new RssParser();
-                        String siteLink = rssParser.getRssLinkFromURL(userLink);
-                        String feedLink = rssParser.getXMLFromURL(userLink);
-                        WebSite webSite = new WebSite();
-                        webSite.setFeedLink(feedLink);
-                        webSite.setSiteLink(siteLink);
-                        rssParser.getWebsiteDetails(webSite, feedLink);
-                        DatabaseHandler databaseHandler = new DatabaseHandler(getApplicationContext());
-                        databaseHandler.insertWebSiteObject(webSite);
-                        // Intent i = new Intent(getApplicationContext(),WebSiteActivity.class);
-                        setResult(100);
-                        finish();
+            if (!editText.toString().isEmpty()) {
+                try {
+                    String userLink = editText.toString();
+                    RssParser rssParser = new RssParser();
+                    String siteLink = rssParser.getRssLinkFromURL(userLink);
+                    String feedLink = rssParser.getXMLFromURL(userLink);
+                    WebSite webSite = new WebSite();
+                    webSite.setFeedLink(feedLink);
+                    webSite.setSiteLink(siteLink);
+                    webSite.getId();
+                    webSite.getTitle();
+                    webSite.getFeedLink();
+                    webSite.getDescription();
+                    webSite.getSiteLink();
+                    DatabaseHandler databaseHandler = new DatabaseHandler(getApplicationContext());
+                    databaseHandler.insertWebSiteObject(webSite);
+                    setResult(100);
+                    finish();
                     } catch (Exception e) {
                         Toast.makeText(getApplicationContext(), "No feeds found", Toast.LENGTH_SHORT);
                     }
                 } else {
-                    Toast.makeText(getApplicationContext(), "Please enter website link", Toast.LENGTH_SHORT).show();
+                       Toast.makeText(getApplicationContext(), "Please enter website link", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
-        private boolean isEmpty(EditText editText){
-            return editText.toString().isEmpty();
-
-
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_web_site, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-
-
-        Button button;
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
