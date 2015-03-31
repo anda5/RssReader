a7e0a31cb806122ac9e17e8708d2650f539559e3
Anda.Lixandroiu <lix.anda@yahoo.com>
2015-03-31 13:08:12 +0300

    Add ViewHolder class ,fixes #98

--- app/src/main/java/com/anda/rssreader/MainActivityAdapter.java
+++ app/src/main/java/com/anda/rssreader/MainActivityAdapter.java
@@ -1,7 +1,10 @@
 package com.anda.rssreader;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
 
 import java.util.List;
 
@@ -13,20 +16,26 @@ public class MainActivityAdapter {
     private Context context;
     private List<WebSite> webSiteList;
 
-
     public MainActivityAdapter(Context context, List<WebSite> websiteList){
         this.context = context;
         this.webSiteList = websiteList;
     }
 
-     public class ViewHolder {
+     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public ViewHolder(){
+         public View itemTitle;
+         public View itemLink;
 
-      }
+         public ViewHolder(View itemView) {
+             super(itemView);
+             itemTitle = itemView.findViewById(R.id.webItemTitle);
+             itemLink = itemView.findViewById(R.id.webItemLink);
+             itemView.setOnClickListener(this);
+         }
 
-
-    }
-
-
+         @Override
+         public void onClick(View v) {
+            //TODO: feed intent
+         }
+     }
 }
