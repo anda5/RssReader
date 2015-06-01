275b643c12c807ed6f56aae94240ff1886933d41
Anda.Lixandroiu <lix.anda@yahoo.com>
2015-06-01 05:05:06 +0300

    Make some change

--- app/src/main/java/com/anda/rssreader/WebSiteActivity.java
+++ app/src/main/java/com/anda/rssreader/WebSiteActivity.java
@@ -24,48 +24,58 @@ public class WebSiteActivity extends Activity {
         setContentView(R.layout.activity_web_site);
         final EditText editText = (EditText) findViewById(R.id.webSiteLink);
 
+
         Button button = (Button) findViewById(R.id.webSiteButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            if (!editText.toString().isEmpty()) {
+             String test = editText.getText().toString();
+            if (!editText.getText().toString().isEmpty()) {
               ProcessWebSite processWebSite = new ProcessWebSite();
+              String name=Utils.fullyQualifiedName(editText.getText().toString());
               processWebSite.execute(Utils.fullyQualifiedName(editText.getText().toString()));
 
                 } else {
-                       Toast.makeText(getApplicationContext(), "Please enter website link", Toast.LENGTH_SHORT).show();
+                  Toast.makeText(getApplicationContext(), "Please enter website link", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
     }
-    private class ProcessWebSite extends AsyncTask<String,Void,Void>{
 
+    private class ProcessWebSite extends AsyncTask<String,Void,Void>{
+        private boolean exception = false;
 
         @Override
         protected Void doInBackground(String... params) {
             try {
+                   if(exception==false) {
+                       String userLink = params[0];
+                       RssParser rssParser = new RssParser();
+                       String rssLink = rssParser.getRssLinkFromURL(userLink);
+                       String rssFeedXML = rssParser.getXMLFromURL(rssLink);
+                       WebSite webSite = new WebSite();
+                       webSite.setFeedLink(rssLink);
+                       webSite.setSiteLink(userLink);
+                       webSite = rssParser.getWebsiteDetails(webSite, rssFeedXML);
+                       DatabaseHandler databaseHandler = new DatabaseHandler(getApplicationContext());
+                       databaseHandler.insertWebSiteObject(webSite);
+                   }
 
-                String userLink = params[0];
-                RssParser rssParser = new RssParser();
-                String  rssLink = rssParser.getRssLinkFromURL(userLink);
-                String  rssFeedXML = rssParser.getXMLFromURL(rssLink);
-                WebSite webSite = new WebSite();
-                webSite.setFeedLink(rssLink);
-                webSite.setSiteLink(userLink);
-                webSite = rssParser.getWebsiteDetails(webSite,rssFeedXML);
-                DatabaseHandler databaseHandler = new DatabaseHandler(getApplicationContext());
-                databaseHandler.insertWebSiteObject(webSite);
             } catch (Exception e) {
-                Toast.makeText(getApplicationContext(), "No feeds found", Toast.LENGTH_SHORT).show();
+                exception = true;
             }
             return null;
-        }
+    }
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            setResult(100);
-            finish();
+            if(exception==true){
+            Toast.makeText(getApplicationContext(), "No feeds found", Toast.LENGTH_SHORT).show();
+            }else {
+                setResult(100);
+                finish();
+            }
         }
     }
 }
